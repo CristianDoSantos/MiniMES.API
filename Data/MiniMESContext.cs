cf9a584b2fa4ff918339753446029421486d9e3c
cristian do santos <cristiandosantos71@gmail.com>
2025-07-16 21:39:19 -0300

    Criando dbContext

---
+++ Data/MiniMESContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniMES.API.Models;
+
+namespace MiniMES.API.Data
+{
+    public class MiniMESContext : DbContext
+    {
+        public MiniMESContext(DbContextOptions<MiniMESContext> options) : base(options) { }
+
+        public DbSet<MaquinaModel> Maquinas => Set<MaquinaModel>();
+        public DbSet<OrdemProducaoModel> Ordens => Set<OrdemProducaoModel>();
+        public DbSet<EventoProducaoModel> Eventos => Set<EventoProducaoModel>();
+    }
+}
--- obj/Debug/net8.0/MiniMES.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/MiniMES.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniMES.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7db593ef78af2748f15ce2792d42afb4c8df29b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+920de199878c20713d02b81a6143ad788c02b9ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniMES.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniMES.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
